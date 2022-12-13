346dede2927602bcc26c9e9fc083cc7de7c5b050
pmtpro <ngatngay@ngatngay.net>
2022-12-13 17:43:04 +0700

    update nettruyen v3

--- nettruyen/src/__core.js
+++ nettruyen/src/__core.js
@@ -1,28 +1,35 @@
-const _server = "https://www.nettruyenmin.com";
-const _oldServer = [
-    "nettruyen.com",
-    "nettruyenmoi.com",
-    "nettruyenone.com",
-    "nettruyentop.com",
-    "nettruyenvip.com",
-    "nettruyenpro.com",
-    "nettruyengo.com",
-    "nettruyenco.com",
-    "nettruyenme.com",
-    "nettruyenin.com",
-    "nettruyenon.com",
-    "nettruyentv.com"
-];
+const _server = "https://www.nettruyenking.com";
 
 const _proxyImage = "https://tooltruyen.ngatngay.net/proxy/nettruyen?url="
 
 function _trueServer(url) {
-    server = _server.split(".");
-    server = server[server.length - 2] + "." + server[server.length - 1];
-
-    _oldServer.forEach(function (value) {
-        url = url.replace(value, server);
-    });
+    url = url.replace(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)/img, _server)
 
     return url;
 }
+
+
+// function _trueServer(url) {
+//     const _oldServer = [
+//     "nettruyen.com",
+//     "nettruyenmoi.com",
+//     "nettruyenone.com",
+//     "nettruyentop.com",
+//     "nettruyenvip.com",
+//     "nettruyenpro.com",
+//     "nettruyengo.com",
+//     "nettruyenco.com",
+//     "nettruyenme.com",
+//     "nettruyenin.com",
+//     "nettruyenon.com",
+//     "nettruyentv.com"
+// ];
+//     server = _server.split(".");
+//     server = server[server.length - 2] + "." + server[server.length - 1];
+
+//     _oldServer.forEach(function (value) {
+//         url = url.replace(value, server);
+//     });
+
+//     return url;
+// }

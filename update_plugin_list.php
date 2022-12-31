fcc980f3840377ceacb5c57cf3b966f97b24540a
pmtpro <ngatngay@ngatngay.net>
2022-12-31 18:35:31 +0700

    update update_plugin_list.php

--- update_plugin_list.php
+++ update_plugin_list.php
@@ -24,45 +24,44 @@ foreach ($plugins as $plugin) {
     // Thong tin plugin
     $plugin_detail = json_decode(file_get_contents($plugin), true);
 
-
-    // Build plugin
-
-    // Xoa plugin cu
-    if (file_exists("$plugin_dir/$plugin_file")) {
-        unlink("$plugin_dir/$plugin_file");
-    }
-
-    // Tao file zip
-    $zip = new ZipArchive();
-    $zip->open("$plugin_dir/$plugin_file", ZipArchive::CREATE | ZipArchive::OVERWRITE);
-
-    $plugin_src_path = realpath($plugin_dir);
-
-    // Create recursive directory iterator
-    /** @var SplFileInfo[] $files */
-    $files = new RecursiveIteratorIterator(
-        new RecursiveDirectoryIterator($plugin_src_path),
-        RecursiveIteratorIterator::LEAVES_ONLY
-    );
-
-    foreach ($files as $name => $file) {
-        if (!$file->isDir()) {
-            // Khong nen plugin file
-            if ($file->getFilename() == $plugin_file) {
-                continue;
-            }
-
-            // Get real and relative path for current file
-            $filePath     = $file->getRealPath();
-            $relativePath = substr($filePath, strlen($plugin_src_path) + 1);
-
-            // Add current file to archive
-            $zip->addFile($filePath, $relativePath);
-        }
-    }
-
-    // Nen file
-    $zip->close();
+    //// Build plugin
+    //
+    //// Xoa plugin cu
+    //if (file_exists("$plugin_dir/$plugin_file")) {
+    //    unlink("$plugin_dir/$plugin_file");
+    //}
+    //
+    //// Tao file zip
+    //$zip = new ZipArchive();
+    //$zip->open("$plugin_dir/$plugin_file", ZipArchive::CREATE | ZipArchive::OVERWRITE);
+    //
+    //$plugin_src_path = realpath($plugin_dir);
+    //
+    //// Create recursive directory iterator
+    ///** @var SplFileInfo[] $files */
+    //$files = new RecursiveIteratorIterator(
+    //    new RecursiveDirectoryIterator($plugin_src_path),
+    //    RecursiveIteratorIterator::LEAVES_ONLY
+    //);
+    //
+    //foreach ($files as $name => $file) {
+    //    if (!$file->isDir()) {
+    //        // Khong nen plugin file
+    //        if ($file->getFilename() == $plugin_file) {
+    //            continue;
+    //        }
+    //
+    //        // Get real and relative path for current file
+    //        $filePath     = $file->getRealPath();
+    //        $relativePath = substr($filePath, strlen($plugin_src_path) + 1);
+    //
+    //        // Add current file to archive
+    //        $zip->addFile($filePath, $relativePath);
+    //    }
+    //}
+    //
+    //// Nen file
+    //$zip->close();
 
     // Thong tin plugin
     $data['data'][] = [
